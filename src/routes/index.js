1cf2f6c4307e56cccb4f208af16d707c22014363
qian.you <qian.you@jutze.com.cn>
2021-09-08 16:36:29 +0800

    featur: about ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,43 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+    isMe: true,
+    blogList: [
+      {
+        id: 0,
+        name: "zhangsan",
+      },
+      {
+        id: 1,
+        name: "lisi",
+      },
+      {
+        id: 2,
+        name: "wangwu",
+      },
+    ],
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
+//动态路径参数:XXX和ctx.params
+router.get("/myindex/:hhh/:hihihi", async (ctx, next) => {
+  const { hhh, hihihi } = ctx.params;
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    hihihi,
+    hhh,
+  };
+});
 
-module.exports = router
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
+
+router.get("/json", async (ctx, next) => {
+  ctx.body = {
+    title: "koa2 json",
+  };
+});
+
+module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,25 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.prefix('/users')
+router.prefix("/users");
+//前缀
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.get("/", function (ctx, next) {
+  ctx.body = "this is a users response!";
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+router.get("/bar", function (ctx, next) {
+  ctx.body = "this is a users/bar response";
+});
 
-module.exports = router
+//post，用postman模拟发送请求
+router.post("/login", async (cxt, next) => {
+  const { userName, password } = cxt.request.body; //请求的body
+  cxt.body = {
+    //响应的body
+    title: "mypage",
+    userName,
+    password,
+  };
+});
+
+module.exports = router;
